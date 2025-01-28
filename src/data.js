607f7b61858bb3d5360095bbe842a6a4af846304
adnan <adnanhaider9993@gmail.com>
2025-01-28 17:21:07 +0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const config = {
+  portfolio: {
+    portfolioLogo: 'Portfolio',
+    projectCard: {
+      cards: [
+        {
+          heading: 'Zaika Recipes FYP',
+          description:
+            'Project description. User can share their own recipies, rate and review dishes  Zaika seamless integration of meal planning tools ensures that users can plan their meals efficiently, saving time and reducing food waste. User can also sell their ingredients through Ecommerce Store. This Website is developed using React JS, NodeJS and Mongodb',
+          animationDelay: '300',
+          buttonLink: 'https://6797d6ae6082f84ccef9d69f--vocal-lollipop-24f532.netlify.app/',
+          buttonText: 'CHECK OUT',
+        },
+        {
+          heading: 'CAR RENTAL website',
+          description:            
+          'Project description. A car rental website is an online platform that allows customers to rent vehicles for a set period of time. Thiswebsite typically includes vehicle inventory, location information and customer support',
+          animationDelay: '500',
+          buttonLink: '',
+          buttonText: 'CHECK OUT',
+        },
+        {
+          heading: 'Coming soon',
+          description: ' ',        
+          animationDelay: '700',
+          buttonLink: '',
+          buttonText: 'CHECK OUT',
+        },
+        // {
+        //   heading: 'Coming Soon',
+        //   description:'',          
+        //   animationDelay: '900',
+        //   buttonLink: '',
+        //   buttonText: 'CHECK OUT',
+        // },
+        // {
+        //   heading: 'Under Construction',
+        //   description: '</>',
+        //   animationDelay: '1100',
+        //   buttonText: '',
+        // },
+      ],
+    },
+    internships: {
+      internship: [
+        {
+          companyName: 'Company Name',
+          role: 'Front-end Developer',
+          introText: '',
+          duration: '29/03/2022 - 29/05/2022',
+          keyPointOne: 'Built front-end of multiple website using ReactJS',
+          keyPointTwo:
+            'Add few lines here....',
+          keyPointThree: 'Add few lines here... if requried Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptas dolo Perspiciatis voluptas do.',
+          keyPointFour:
+            'Add few lines here... if requried Lorem, ipsum dolor sit amet consectetur adipisicing architecto molestiae.',
+          offsetValue: '0',
+        },
+        {
+          companyName: 'Company Name',
+          role: 'Front-end Developer',
+          introText: '',
+          duration: '14/06/2021 - 14/07/2021',
+          keyPointOne: 'Built front-end of multiple website using ReactJS.',
+          keyPointTwo: 'Add few lines here... if requried',
+          keyPointThree:
+            'Add few lines here... if requried',
+          keyPointFour:
+            'Add few lines here... if requried Lorem, ipsum dolor sit amet consectetur adipisicing cto molestiae.',
+          offsetValue: '-1000',
+        },
+        {
+          companyName: 'Company Name',
+          role: 'Front-end Developer',
+          introText: '',
+          duration: '10/09/2020 - 10/11/2020',
+          keyPointOne:
+            'Add few lines here... if requried',
+          keyPointTwo: 'Add few lines here... if requried',
+          keyPointThree: 'Add few lines here... if requried',
+          keyPointFour: '',
+          offsetValue: '-1000',
+        },
+      ],
+    },
+  },
+};
+
+export default config;
